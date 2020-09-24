a0ab8662c893bc0b26a96c4c9766d6aac1dfe01a
Rob Parker <RobertMParker@gmail.com>
2020-09-24 08:17:26 -0700

    Add KCAPACIF205 station

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 window.onload = function() {
   new WindCharts('KCADALYC37');
   new WindCharts('KCADALYC1');
+  new WindCharts('KCAPACIF205');
   new WindCharts('KCASANFR69');
   new TideChart('9414290');
 };
 
 // TODO: Refresh the page on a set interval.  Maybe every 10 minutes.
-
-// TODOs to verify conditions
-// Check data is fairly recent
-// Check data is not volatile
-// Check data falls within acceptable bounds
-// Check data correlates with other weather stations
-// Check rain forecast
-// Check fog - how to do this?  Image analysis?
-// Check tide conditions?
