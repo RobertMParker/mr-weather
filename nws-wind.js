baf0069729764e169781d4afda9a90db9c46c79e
Rob Parker <robparker@google.com>
2021-02-05 09:44:24 -0800

    Filter out old data and too future data from NWS wind forecast.  This makes more relevant data stand out better.

--- nws-wind.js
+++ nws-wind.js
@@ -19,12 +19,20 @@ class NWSWind extends Wind {
         var windDir = [];
         var windSpeed = [];
         var windGust = [];
+        
+        var minMoment = moment().subtract(3, 'hours');
+        var maxMoment = moment().add(72, 'hours');
+          
         for (var i=0; i<response.properties.periods.length; i++) {
           var forecast = response.properties.periods[i];
           var forecastMoment = moment(forecast.startTime);
+          
+          // Filter out data that is too far in the past or too far in the future
+          if (!forecastMoment.isAfter(minMoment) || forecastMoment.isAfter(maxMoment) ) continue;
 
           windDir.push({x: forecastMoment, y: this.getCardinal(forecast.windDirection)});
           windSpeed.push({x: forecastMoment, y: Math.round(forecast.windSpeed.value / 1.60934)});
+          // TODO(robparker): This data is getting a null value for windGust, maybe should just take it out
           windGust.push({x: forecastMoment, y: forecast.windGust ? Math.round(forecast.windGust.maxValue / 1.60934) : NaN});
         }
         this.createChart(windSpeed, windGust, windDir, true);
