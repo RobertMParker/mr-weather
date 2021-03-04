2795d1bb630f1078bf319755e1df50ca3821ac5e
Rob Parker <robparker@google.com>
2021-03-03 21:45:58 -0800

    Add Firebase Performance Monitoring

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDWGTw5FhOgh1MjwGDEvtI6zXc0aU1mpjQ",
+  authDomain: "mussel-rock-weather.firebaseapp.com",
+  projectId: "mussel-rock-weather",
+  storageBucket: "mussel-rock-weather.appspot.com",
+  messagingSenderId: "851310048475",
+  appId: "1:851310048475:web:307603acf456d62b7688fe"
+};
+
+// Initialize Firebase and performance monitoring
+firebase.initializeApp(firebaseConfig);
+var perf = firebase.performance();
